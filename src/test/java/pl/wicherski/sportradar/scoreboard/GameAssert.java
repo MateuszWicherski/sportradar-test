9b6daef13abed5daf7f6ece4c3fdef03ef05a859
Mateusz Wicherski <mateusz@wicherski.pl>
2022-09-18 20:00:14 +0200

    Add tests for updating Game score

---
+++ src/test/java/pl/wicherski/sportradar/scoreboard/GameAssert.java
@@ -0,0 +1,35 @@
+package pl.wicherski.sportradar.scoreboard;
+
+import org.assertj.core.api.AbstractAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class GameAssert extends AbstractAssert<GameAssert, Game> {
+
+    private GameAssert(Game game) {
+        super(game, GameAssert.class);
+    }
+
+    public static GameAssert assertThatGame(Game game) {
+        return new GameAssert(game);
+    }
+
+    public GameAssert hasHomeTeam(String homeTeam) {
+        isNotNull();
+        assertThat(actual.homeTeamName()).isEqualTo(homeTeam);
+        return this;
+    }
+
+    public GameAssert hasAwayTeam(String awayTeam) {
+        isNotNull();
+        assertThat(actual.awayTeamName()).isEqualTo(awayTeam);
+        return this;
+    }
+
+    public GameAssert hasScore(Score score) {
+        isNotNull();
+        assertThat(actual.score()).isEqualTo(score);
+        return this;
+    }
+
+}
